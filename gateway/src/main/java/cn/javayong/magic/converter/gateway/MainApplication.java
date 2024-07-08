4d81e875f6250dd91b2ec82bee76f3e2f6352908
zhangyong <zhangyong7120180@163.com>
2024-07-09 00:22:33 +0800

    初次部署

---
+++ gateway/src/main/java/cn/javayong/magic/converter/gateway/MainApplication.java
@@ -0,0 +1,7 @@
+package cn.javayong.magic.converter.gateway;
+
+public class MainApplication {
+
+
+
+}
---
+++ worker/src/main/java/cn/javayong/magic/converter/worker/MainApplication.java
@@ -0,0 +1,7 @@
+package cn.javayong.magic.converter.worker;
+
+public class MainApplication {
+
+
+
+}
