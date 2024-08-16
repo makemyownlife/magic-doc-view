3324983d2dc08cb790a50e503a316001199f6cac
张勇 <zhangyong7120180@163.com>
2024-08-16 17:39:44 +0800

    优化网关和工作服务信息

--- gateway/src/main/java/cn/javayong/magic/converter/gateway/GatewayApplication.java
+++ gateway/src/main/java/cn/javayong/magic/converter/gateway/GatewayApplication.java
@@ -2,6 +2,8 @@ package cn.javayong.magic.converter.gateway;
 
 public class GatewayApplication {
 
-
-
+    public static void main(String[] args) {
+        System.out.println("hello gateway!!");
+    }
+    
 }
