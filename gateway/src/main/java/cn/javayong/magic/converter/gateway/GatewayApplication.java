2afd9f64fef17fc686abc45a56ffdbdeeba8b2a7
zhangyong <zhangyong7120180@163.com>
2024-08-17 21:00:44 +0800

    新建 gateway package-info

--- gateway/src/main/java/cn/javayong/magic/converter/gateway/GatewayApplication.java
+++ gateway/src/main/java/cn/javayong/magic/converter/gateway/GatewayApplication.java
@@ -1,11 +1,18 @@
 package cn.javayong.magic.converter.gateway;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 网关开发(接口提交 、文件渲染)
  */
+@EnableTransactionManagement
+@SpringBootApplication
+@MapperScan("cn.javayong.magic.converter")
 public class GatewayApplication {
 
     private static Logger logger = LoggerFactory.getLogger(GatewayApplication.class);
@@ -13,6 +20,8 @@ public class GatewayApplication {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         logger.info("开始启动转码网关");
+        SpringApplication application = new SpringApplication(GatewayApplication.class);
+        application.run(args);
         logger.info("结束启动转码网关，耗时：" + (System.currentTimeMillis() - start) + "毫秒");
     }
 
---
+++ gateway/src/main/java/cn/javayong/magic/converter/gateway/service/package-info.java
@@ -0,0 +1,1 @@
+package cn.javayong.magic.converter.gateway.service;
