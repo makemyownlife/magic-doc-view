4d58564d6afdff36fce7274a423014a07d7e717a
张勇 <zhangyong7120180@163.com>
2024-08-17 10:51:27 +0800

    整体网关模块，打印日志

--- gateway/src/main/java/cn/javayong/magic/converter/gateway/GatewayApplication.java
+++ gateway/src/main/java/cn/javayong/magic/converter/gateway/GatewayApplication.java
@@ -1,9 +1,19 @@
 package cn.javayong.magic.converter.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 网关开发(接口提交 、文件渲染)
+ */
 public class GatewayApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(GatewayApplication.class);
+
     public static void main(String[] args) {
-        System.out.println("hello gateway!!");
+        long start = System.currentTimeMillis();
+        logger.info("开始启动转码网关");
+        logger.info("结束启动转码网关，耗时：" + (System.currentTimeMillis() - start) + "毫秒");
     }
-    
+
 }
